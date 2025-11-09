dbe449967031741730c31ead6cfe957cc9b356b8
Gabriel Nunes Martins <gabrielnnsmartins@hotmail.com>
2025-11-08 23:55:13 -0300

    Struct com personagem e parte do sistema que implementa a movimentação

--- src/main.c
+++ src/main.c
@@ -1,7 +1,27 @@
-#include "raylib.h"
+#include <raylib.h>
+
+typedef struct protagonista{
+    Vector2 position;
+    Vector2 target_position;
+    float speed;
+
+    int vidas;
+    int moedas;
+    int forca;
+
+    int frame_atual;
+    int contador_frames;
+    int loop_frames;
+    int frames_por_loop;
+    bool movimento;
+} protagonista;
+
+
+
+
 
 int main() {
-    InitWindow(800, 450, "Meu Jogo");
+    InitWindow(800, 450, "Bonded");
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
